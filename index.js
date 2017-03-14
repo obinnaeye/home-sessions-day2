52f3abc7ddb1c8d573f358cddefc0ade24b22d3a
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-03-14 10:24:19 +0000

    implement fs.writeFile to output

--- index.js
+++ index.js
@@ -15,17 +15,31 @@ module.exports = function(){
             if(err) throw err;
             var obj = JSON.parse(res.body);
             var weda = {
+                "Search-for" : input,
                 "Reference-City": obj.name,
                 "Summary": obj.weather[0].main + "; " + obj.weather[0].description,
                 "Temperature" : Math.round(obj.main.temp - 273) + " Celcius",
                 "Pressure" : Math.round(obj.main.pressure) + " hPa",
                 "Humidity" : Math.round(obj.main.humidity) + " %"
-                
-                
+            };
+            
+            var data = JSON.stringify(weda, null, 2);
+            
+            
+            if (output){
+                fs.writeFile(output, data, function(err, d){
+                    if (err) throw err;
+                    console.log("Weather saved in " + output);
+                })
+            }else{
+                fs.writeFile("output.txt", data, function(err, d){
+                    if (err) throw err;
+                    console.log("Weather saved in " + "output.txt!");
+                })
+                //use JSON.stringify(weda, null, 2) to print pretty on console.
+                console.log("The weather in " + input + " is","\n", data);
             }
             
-            //use JSON.stringify(weda, null, 2) to print pretty on console.
-            console.log("The weather in " + input + " is","\n",JSON.stringify(weda, null, 2));
         })
     }else{
         console.log("Please specified the city using -i 'city'");
