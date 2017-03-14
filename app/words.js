084dba7cac62f901cb6143bae100eb9670e9dbca
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-03-14 13:55:02 +0000

    pass all tests except the last

--- app/words.js
+++ app/words.js
@@ -8,11 +8,16 @@ module.exports = {
                 var obj = {};
                 console.log(wordArr);
                 for (var i =0; i<wordArr.length; i++){
-                    obj[wordArr[i]] = obj[wordArr[i]] +1 || 1;
+                    var value = obj[wordArr[i]];
+                    if(typeof value === "function"){
+                        var value = 1;
+                    }
+                    console.log (value);
+                    obj[wordArr[i]] = value +1 || 1;
                 }
                 
                 console.log(obj);
-                //return obj;
+                return obj;
             }else{
                 console.log("Not inpu")
             }
