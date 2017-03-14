dd1995b1ecb6a4cb054239ef46372107fe6ae1c1
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-03-14 14:25:37 +0000

    pass test cases except for null

---
+++ app/reverse.js
@@ -0,0 +1,30 @@
+module.exports = {
+    reverseString : function(st) {
+        if(typeof st === "string"){
+            if(palindrome(st)){
+                return true;
+            }else{
+                return reverseStr(st);
+            }
+            
+        }
+        
+        function palindrome(str) {
+          var joined = str.replace(/\W+|_/g, "");
+          var lowJoin = joined.toLowerCase();
+          var revJoin = lowJoin.split("").reverse().join("");
+          if (revJoin === lowJoin){
+            return true;
+          }
+          else {
+            return false;
+          }
+        }
+        
+        function reverseStr(str) {
+          var array = str.split("").reverse();
+          return array.join('');
+        }
+
+    }
+}
